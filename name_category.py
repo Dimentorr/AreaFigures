2ef34b13b058c106bd4c44070f81a6b21f84b01e
Петрук Степан Сергеевич <stepapetruk@ya.ru>
2023-09-15 10:58:53 +1000

    DF return

--- name_category.py
+++ name_category.py
@@ -1,21 +1,8 @@
 import findspark
-import pyspark
-from pyspark.sql import SparkSession, SQLContext
-from pyspark import SparkConf, SparkContext
+findspark.init()
+
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 from pyspark.shell import spark
-
-from pyspark.sql import SparkSession
-
-# scSpark = SparkSession \
-#     .builder \
-#     .appName("Python Spark SQL basic example: Reading CSV file without mentioning schema") \
-#     .config("spark.some.config.option", "some-value") \
-#     .getOrCreate()
-#
-# sdfData = scSpark.read.csv("product.csv", header=True, sep=";")
-# sdfData.show()
-
 
 schema_product = StructType(fields=[
     StructField("id", IntegerType()),
@@ -49,18 +36,6 @@ df_product_with_category = spark.read.csv(
     sep=";",
     header=True)
 
-# работает, но для экономии времени закоментированно
-# df_product.show()
-# df_category.show()
-# df_product_with_category.show()
-
-# tmp = [[] for i in df_product_with_category.collect()]
-prod = dict(map(lambda x: (x.id, x.name), df_product.collect()))
-categor = dict(map(lambda x: (x.id, x.name), df_category.collect()))
-prod_with_category = map(lambda x: (int(x.id_product), int(x.id_category)), df_product_with_category.collect())
-
-print('-----------------------------------------------')
-
 
 def ids_in_values(list_with_dict_ids, dick_for_key, dict_for_values):
     answer = dict()
@@ -68,24 +43,23 @@ def ids_in_values(list_with_dict_ids, dick_for_key, dict_for_values):
         if dick_for_key[i[0]] not in answer.keys():
             answer[dick_for_key[i[0]]] = dict_for_values[i[1]]
         else:
-            answer[dick_for_key[i[0]]] = answer[dick_for_key[i[0]]], dict_for_values[i[1]]
+            answer[dick_for_key[i[0]]] = f'{answer[dick_for_key[i[0]]]}, {dict_for_values[i[1]]}'
     return answer
 
 
-# answer = dict(map(lambda x: (prod[x[0]], categor[x[1]]) if(x[0] not in answer.keys()) else (
-#     prod[x[0]], answer[prod[x[0]]] + f', {categor[x[1]]}'), prod_with_category))
-print(ids_in_values(prod_with_category, prod, categor))
+def return_all_product_with_category(df_products, df_categories, df_relationships):
+    product = dict(map(lambda x: (x.id, x.name), df_products.collect()))
+    category = dict(map(lambda x: (x.id, x.name), df_categories.collect()))
+    prod_with_category = map(lambda x: (int(x.id_products), int(x.id_categories)), df_relationships.collect())
+
+    pre_answer = ids_in_values(prod_with_category, product, category)
+    new_keys = set(product.values()) - set(pre_answer.keys())
+    pre_answer = pre_answer | dict(map(lambda x: (x, ''), new_keys))
+    answer = list(map(lambda x: (x, pre_answer[x]), pre_answer))
+
+    columns = ['products', 'categories']
+    df = spark.createDataFrame(answer, columns)
+    return df
 
 
-# answer = list()
-# for i in df_product_with_category.collect():
-#     answer.append(dict(map(lambda product, category: (product[i.id_product], category[i.id_category]))))
-# for i in answer:
-#     print(i)
-# print(df_product.collect())
-
-# print(df_product.select(df_product.name.count()).where(df_product.id == i.id_product))
-# context = f"SELECT df_product.name FROM df_product WHERE df_product.id = {i.id_product}"
-# print(SQLContext.sql(sqlQuery=context))
-
-# sql_test = SQLContext.sql('select df_product.name from df_product where df_product.id =  ')
+return_all_product_with_category(df_product, df_category, df_product_with_category).show()
